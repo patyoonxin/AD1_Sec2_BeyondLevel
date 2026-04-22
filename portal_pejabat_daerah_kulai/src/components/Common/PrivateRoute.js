ffb61e670bfb5b102388beb0bb06ecbc7a32c6d7
irdina <irdinastudy@gmail.com>
2026-04-22 10:09:35 +0800

    Delete Old File

--- portal_pejabat_daerah_kulai/src/components/Common/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-function PrivateRoute({ user, children }) {
-  return user ? children : <Navigate to="/login" />;
-}
-
-export default PrivateRoute;
